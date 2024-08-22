018e41070d9bd0a64dfee3cfb5472333af58973e
filyys <filipsysak@gmail.com>
2024-08-22 23:11:32 +0200

    Notification update & Tailwind Prettier plugin addition

--- src/App.tsx
+++ src/App.tsx
@@ -21,90 +21,97 @@ import settings from "./assets/icons/settings.webp";
 
 function App() {
   return (
-    <div className="h-screen w-screen text-white select-none">
-      <nav className="w-screen h-max font-[500] flex flex-row justify-between items-center shadow-sm z-10 [text-shadow:_0px_0px_5px_rgb(0_0_0_/_30%)] bg-gradient-to-r from-[#363b87] to-[#3058b6] via-[#3952a7] ">
+    <div className="h-screen w-screen select-none text-white">
+      <nav className="z-10 flex h-max w-screen flex-row items-center justify-between bg-gradient-to-r from-[#363b87] via-[#3952a7] to-[#3058b6] font-[500] shadow-sm [text-shadow:_0px_0px_5px_rgb(0_0_0_/_30%)]">
         <div className="flex flex-row items-center">
           <div className="px-5">
             <AppleIcon />
           </div>
 
-          <ul className="flex flex-row h-fit w-fit text-[13px] text-center  py-1">
-            <li className="flex items-center h-[24px] rounded-[4px] hover:bg-white/[.2] font-bold px-2 py-1">
+          <ul className="flex h-fit w-fit flex-row py-1 text-center text-[13px]">
+            <li className="flex h-[24px] items-center rounded-[4px] px-2 py-1 font-bold hover:bg-white/[.2]">
               Finder
             </li>
-            <li className="flex items-center h-[24px] rounded-[4px] hover:bg-white/[.2] px-[11px] py-1">
+            <li className="flex h-[24px] items-center rounded-[4px] px-[11px] py-1 hover:bg-white/[.2]">
               File
             </li>
-            <li className="flex items-center h-[24px] rounded-[4px] hover:bg-white/[.2] px-[11px] py-1">
+            <li className="flex h-[24px] items-center rounded-[4px] px-[11px] py-1 hover:bg-white/[.2]">
               Edit
             </li>
-            <li className="flex items-center h-[24px] rounded-[4px] hover:bg-white/[.2] px-[11px] py-1">
+            <li className="flex h-[24px] items-center rounded-[4px] px-[11px] py-1 hover:bg-white/[.2]">
               View
             </li>
-            <li className="flex items-center h-[24px] rounded-[4px] hover:bg-white/[.2] px-[11px] py-1">
+            <li className="flex h-[24px] items-center rounded-[4px] px-[11px] py-1 hover:bg-white/[.2]">
               Go
             </li>
-            <li className="flex items-center h-[24px] rounded-[4px] hover:bg-white/[.2] px-[11px] py-1">
+            <li className="flex h-[24px] items-center rounded-[4px] px-[11px] py-1 hover:bg-white/[.2]">
               Window
             </li>
-            <li className="flex items-center h-[24px] rounded-[4px] hover:bg-white/[.2] px-[11px] py-1">
+            <li className="flex h-[24px] items-center rounded-[4px] px-[11px] py-1 hover:bg-white/[.2]">
               Help
             </li>
           </ul>
         </div>
 
         <div className="flex flex-row">
-          <ul className="flex flex-row h-5 w-fit items-center justify-center gap-2">
+          <ul className="flex h-5 w-fit flex-row items-center justify-center gap-2">
             <li>
-              <div className="rounded-sm hover:bg-white/[.2] p-1">
+              <div className="rounded-sm p-1 hover:bg-white/[.2]">
                 <WifiIcon />
               </div>
             </li>
             <li>
-              <div className="rounded-sm hover:bg-white/[.2] p-1">
+              <div className="rounded-sm p-1 hover:bg-white/[.2]">
                 <SearchIcon />
               </div>
             </li>
             <li>
-              <div className="rounded-sm hover:bg-white/[.2] p-1">
+              <div className="rounded-sm p-1 hover:bg-white/[.2]">
                 <AccountIcon />
               </div>
             </li>
             <li>
-              <div className="rounded-sm hover:bg-white/[.2] p-1">
+              <div className="rounded-sm p-1 hover:bg-white/[.2]">
                 <MenuIcon />
               </div>
             </li>
           </ul>
 
-          <div className="text-[13px] text-center  px-5 [text-shadow:_0px_0px_5px_rgb(0_0_0_/_30%)]">
+          <div className="px-5 text-center text-[13px] [text-shadow:_0px_0px_5px_rgb(0_0_0_/_30%)]">
             Mon Jun 5 9:41 AM
           </div>
         </div>
       </nav>
 
-      <div className="w-screen h-fit absolute top-0 left-0 flex items-center justify-center pt-12 text-[#252525] pointer-events-none">
-        <div className="w-[344px] h-[77px] p-3 bg-[#f6f6f6] rounded-2xl drop-shadow-lg gap-2 flex">
-          <div className="max-w-md h-max text-sm">
+      <div className="pointer-events-none absolute left-0 top-0 flex h-fit w-screen items-center justify-center pt-12 text-[#252525]">
+        <div className="flex w-[344px] gap-2 rounded-2xl bg-[#f6f6f6] p-3 drop-shadow-[0px_0px_10px_rgb(0_0_0_/_30%)]">
+          <div className="flex min-w-10 items-center justify-center">
+            <img
+              src={safari}
+              alt="settings-icon"
+              className="size-10 drop-shadow-sm"
+            />
+          </div>
+          <div className="w-fit text-xs">
             <p className="font-bold">Title goes here</p>
             <p className="font-medium leading-4">
               This is the description of the notification and it is very long
             </p>
           </div>
-          <div className="w-8 h-max relative">
-            <p className="absolute top-0 right-0 text-zinc-200 text-sm">now</p>
+          <div className="relative w-8">
+            <p className="absolute right-0 top-0 text-xs text-zinc-200">now</p>
           </div>
         </div>
       </div>
 
-      <div className="w-screen h-fit absolute bottom-0 left-0 flex items-center justify-center pb-1">
-        <div className="w-fit h-[65px] backdrop-blur-2xl rounded-2xl border-[1px] border-white/10 backdrop-brightness-[.8]">
+      <div className="absolute bottom-0 left-0 flex h-fit w-screen items-center justify-center pb-1">
+        <div className="h-[65px] w-fit rounded-2xl border-[1px] border-white/10 backdrop-blur-2xl backdrop-brightness-[.8]">
           <ul className="flex flex-row justify-between gap-[2px] pt-1">
             <li>
               <div className="h-[60px] pl-1">
                 <img src={finder} alt="finder" style={{ width: "50px" }} />
-                <div className="w-full h[10px] flex items-center justify-center">
-                  <div className="w-[4px] h-[4px] bg-gray-400 opacity-75 rounded-full" />
+                <div className="h[10px] flex w-full items-center justify-center">
+                  <div className="h-[4px] w-[4px] rounded-full bg-gray-400 opacity-75" />
                 </div>
               </div>
             </li>
@@ -142,7 +149,7 @@ function App() {
         </div>
       </div>
 
-      <div className="absolute top-0 left-0 z-[-10] h-dvh w-full bg-cover bg-center bg-no-repeat">
+      <div className="absolute left-0 top-0 z-[-10] h-dvh w-full bg-cover bg-center bg-no-repeat">
         <img src={imgWallpaper} alt="wallpaper" />
       </div>
     </div>
